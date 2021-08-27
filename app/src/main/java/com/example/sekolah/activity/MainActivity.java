df75ada418ad3c086d64ed7cebc2aa168f3eb6a7
mohammadsulaeman <sulaemanm991@gmail.com>
2021-08-27 10:15:14 +0700

    first commit

---
+++ app/src/main/java/com/example/sekolah/activity/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.sekolah.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.sekolah.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn=(Button)findViewById(R.id.lihatbtn);
+        Button btn2=(Button)findViewById(R.id.inputbtn);
+        Button btn3=(Button)findViewById(R.id.infobtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent inte = new Intent(MainActivity.this, datamahasiswa.class);
+                startActivity(inte);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent inte = new Intent(MainActivity.this, inputdata.class);
+                startActivity(inte);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent inte = new Intent(MainActivity.this,informasi .class);
+                startActivity(inte);
+            }
+        });
+    }
+}
